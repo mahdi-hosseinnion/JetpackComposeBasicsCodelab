55390a31d6a7e2660ded19c3df412227964bcd79
mahdi <ssmmhh1382@gmail.com>
2022-05-05 23:47:13 +0430

    finish lesson #5 & 6: reusing composable & Creating column and rows

--- app/src/main/java/com/ssmmhh/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/ssmmhh/jetpackcomposebasicscodelab/MainActivity.kt
@@ -3,14 +3,15 @@ package com.ssmmhh.jetpackcomposebasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ssmmhh.jetpackcomposebasicscodelab.ui.theme.JetpackComposeBasicsCodelabTheme
@@ -21,31 +22,53 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposeBasicsCodelabTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("there!")
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(
-            modifier = Modifier.padding(all = 8.dp),
-            text = "Hello $name!"
-        )
+fun MainScreen(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    JetpackComposeBasicsCodelabTheme {
-        Greeting("Android")
+private fun Greeting(name: String) {
+    Surface(
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .fillMaxWidth(),
+        color = MaterialTheme.colors.primary
+    ) {
+        Row(modifier = Modifier) {
+            Column(
+                modifier = Modifier
+                    .padding(24.dp)
+                    .weight(10f)
+            ) {
+
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(16.dp),
+                onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
+        }
     }
+}
+
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun MainScreenPreview() {
+    MainScreen()
 }
--- app/src/main/java/com/ssmmhh/jetpackcomposebasicscodelab/ui/theme/Theme.kt
+++ app/src/main/java/com/ssmmhh/jetpackcomposebasicscodelab/ui/theme/Theme.kt
@@ -13,7 +13,11 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary =
+
+
+
+    Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
 
