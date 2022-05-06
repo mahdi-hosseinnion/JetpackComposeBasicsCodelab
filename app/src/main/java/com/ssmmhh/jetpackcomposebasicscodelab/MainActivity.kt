44b20eb8b57a9cfa807f3c97b791d19be776ca59
mahdi <ssmmhh1382@gmail.com>
2022-05-06 23:33:14 +0430

    finish lesson #11 & #12: Animation and styling

--- app/src/main/java/com/ssmmhh/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/ssmmhh/jetpackcomposebasicscodelab/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.ssmmhh.jetpackcomposebasicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ssmmhh.jetpackcomposebasicscodelab.ui.theme.JetpackComposeBasicsCodelabTheme
@@ -71,31 +77,43 @@ private fun Greetings(names: List<String> = List(1_000) { "#$it" }) {
 @Composable
 private fun Greeting(name: String) {
 
-    val expanded = rememberSaveable { mutableStateOf(false) }
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
-    val extraPadding = if (expanded.value) 56.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded) 56.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         modifier = Modifier
             .padding(vertical = 4.dp, horizontal = 8.dp)
             .fillMaxWidth(),
+        shape = RoundedCornerShape(8.dp),
         color = MaterialTheme.colors.primary
     ) {
-        Row(modifier = Modifier) {
+        Row(modifier = Modifier.padding(8.dp)) {
             Column(
                 modifier = Modifier
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
                     .weight(10f)
             ) {
 
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             OutlinedButton(
                 modifier = Modifier
                     .padding(16.dp),
-                onClick = { expanded.value = !expanded.value }) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+                onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
     }
@@ -105,5 +123,33 @@ private fun Greeting(name: String) {
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun MainScreenPreview() {
-    MainScreen()
+    JetpackComposeBasicsCodelabTheme {
+        MainScreen()
+    }
+}
+
+@Preview(
+    showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES,
+)
+@Composable
+private fun MainScreenPreviewDark() {
+    JetpackComposeBasicsCodelabTheme {
+        MainScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun GreetingPreview() {
+    JetpackComposeBasicsCodelabTheme {
+        Greeting("Mahdi")
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun GreetingPreviewDark() {
+    JetpackComposeBasicsCodelabTheme {
+        Greeting("Mahdi")
+    }
 }
