5ef305fa8e09b564d8702c6f5caf290cfa984632
mahdi <ssmmhh1382@gmail.com>
2022-05-07 00:09:36 +0430

    finish lesson Jetpack Compose basics code lab

--- app/src/main/java/com/ssmmhh/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/ssmmhh/jetpackcomposebasicscodelab/MainActivity.kt
@@ -4,22 +4,26 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ssmmhh.jetpackcomposebasicscodelab.ui.theme.JetpackComposeBasicsCodelabTheme
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,25 +83,24 @@ private fun Greeting(name: String) {
 
     var expanded by rememberSaveable { mutableStateOf(false) }
 
-    val extraPadding by animateDpAsState(
-        if (expanded) 56.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-
-    Surface(
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier
             .padding(vertical = 4.dp, horizontal = 8.dp)
             .fillMaxWidth(),
-        shape = RoundedCornerShape(8.dp),
-        color = MaterialTheme.colors.primary
     ) {
-        Row(modifier = Modifier.padding(8.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+        ) {
             Column(
                 modifier = Modifier
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
                     .weight(10f)
             ) {
 
@@ -108,12 +111,25 @@ private fun Greeting(name: String) {
                         fontWeight = FontWeight.ExtraBold
                     )
                 )
+                val randomText = remember { UUID.randomUUID().toString().repeat(6) }
+                if (expanded)
+                    Text(
+                        text = randomText,
+                        style = MaterialTheme.typography.caption
+                    )
             }
-            OutlinedButton(
+            IconButton(
                 modifier = Modifier
                     .padding(16.dp),
                 onClick = { expanded = !expanded }) {
-                Text(text = if (expanded) "Show less" else "Show more")
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
             }
         }
     }
